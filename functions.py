e5e56ad9cfa66fd7107a2b78c412f13975c73917
lfalc <lorenzfalcioni@gmail.com>
2023-12-05 20:48:31 +0100

    get_gaze_by_stimulus nearly working

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 
+
 def get_AOI_hits(data: pd.DataFrame) -> dict:
     """Returns a dictionary containing AOIs and timestamps of their samples.
     """
@@ -12,12 +13,13 @@ def get_AOI_hits(data: pd.DataFrame) -> dict:
         for AOI in AOIs:
             for col in data.columns:
                 if (col == AOI) and (row[1][col] == 1):
-                    AOI_hits[AOI]['timestamps'].append(row[1]['Recording timestamp'])
-    
+                    AOI_hits[AOI]['timestamps'].append(
+                        row[1]['Recording timestamp'])
+
     # add number of hits to the dictionary
     for AOI in AOI_hits.keys():
         AOI_hits[AOI]['hits'] = len(AOI_hits[AOI]['timestamps'])
-    
+
     return AOI_hits
 
 # data = pd.read_csv('trim.tsv', sep='\t', header=0)
@@ -25,9 +27,30 @@ def get_AOI_hits(data: pd.DataFrame) -> dict:
 # AOI_hits = get_AOI_hits(data)
 # json.dump(AOI_hits, open('AOI_hits.json', 'w'))
 
+
 def get_gaze_by_stimulus(data: pd.DataFrame) -> dict:
     """Returns a list of dictionarys containing gaze coordinates for each stimulus.
     """
+    # drop rows with Event "MouseEvent"
+    data = data[data['Event'] != 'MouseEvent']
+
     gaze_by_stimulus = []
-    stimuli = data['StimulusName'].unique()
-    
+
+    for participant in data['Participant name'].unique():
+        data_participant = data[data['Participant name'] == participant]
+        for event in data_participant['Event value'].unique():
+            start_index = data_participant[(data_participant['Event value'] == event) &
+                                           (data_participant['Event'] == 'ImageStimulusStart')]
+            end_index = data_participant[(data_participant['Event value'] == event) &
+                                         (data_participant['Event'] == 'ImageStimulusEnd')]
+            print(f"Participant: {participant}, Event: {event}")
+            gaze = data_participant[(data_participant['Recording timestamp'] >= start_index['Recording timestamp'].values[0]) &
+                                    (data_participant['Recording timestamp'] <= end_index['Recording timestamp'].values[0])]
+            gaze_by_stimulus.append({'participant': participant,
+                                     'event': event,
+                                     'gaze': gaze})
+
+
+data = pd.read_csv('trim.tsv', sep='\t', header=0)
+gaze_by_stimulus = get_gaze_by_stimulus(data)
+print(gaze_by_stimulus[0]['gaze'].head())
