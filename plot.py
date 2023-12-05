1629d956aa05a699497b25d0a27ce9c78b3cc8a5
Markus0208 <markus.schmidhuber@st.oth-regensburg.de>
2023-12-05 21:37:13 +0100

    Issue beseitigt

--- plot.py
+++ plot.py
@@ -4,14 +4,17 @@ import functions as f
 def plot_AOI_hits(AOI_hits):
     """Plots AOI hits.
     """
-    fig = plt.figure(figsize=(25, 10))
+    fig = plt.figure(figsize=(30, 15))  # Increase the size of the figure
     ax = fig.add_subplot(111)
     labels = []
     for i, (key, value) in enumerate(AOI_hits.items()):
         ax.bar(i, value['hits'], label=key)
-        labels.append(key)
+        label = key.split('AOI hit ')[1]  # Split the key at 'AOI hit ' and keep only the second part
+        label = label.replace('[', '').replace(']', '')  # Remove square brackets
+        labels.append(label)
     ax.set_xticks(range(len(AOI_hits)))
-    ax.set_xticklabels(labels, rotation=45, fontsize=12)
+    ax.set_xticklabels(labels, rotation=45, fontsize=12, ha='right')  # Set horizontal alignment to 'right'
+    plt.tight_layout()  # Adjust the layout to make room for the x-axis labels
     return fig
 
 import json
