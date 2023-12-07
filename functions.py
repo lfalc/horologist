b4c5219afac4ee334295808715660f3df4bc7370
lfalc <lorenzfalcioni@gmail.com>
2023-12-07 09:09:45 +0100

    Remove comments

--- functions.py
+++ functions.py
@@ -40,36 +40,21 @@ def get_gaze_by_stimulus(data: pd.DataFrame) -> list:
     for participant in data['Participant name'].unique():
         print('Participant: ', participant)
         data_participant = data[data['Participant name'] == participant].copy()
-        # print(data_participant.shape[0])
         for event in stimuli:
             start_index = data_participant[(data_participant['Event value'] == event) &
                                            (data_participant['Event'] == 'ImageStimulusStart')]
             end_index = data_participant[(data_participant['Event value'] == event) &
                                          (data_participant['Event'] == 'ImageStimulusEnd')]
-            # print start and end index
-            # print(start_index.index[0], end_index.index[0])
 
             gaze = data_participant[start_index.index[0]:end_index.index[0]].copy()
-            # print(gaze.isna().sum())
-            # print(gaze.shape[0])
-            # drop samples in which either gaze point X or gaze point Y is NaN
             gaze = gaze.dropna(subset=['Gaze point X', 'Gaze point Y'])
-
-            # convert nan to 0 in gaze point X and Y
-            # gaze['Gaze point X'] = gaze['Gaze point X'].fillna(0)
-            # gaze['Gaze point Y'] = gaze['Gaze point Y'].fillna(0)
-               
-            # print(f"Gaze size: {gaze.shape[0]}")  # Print the size of the gaze DataFrame
-            # print(gaze.head())
 
             gaze_by_stimulus.append({'participant': str(participant),
                                      'event': str(event),
                                      'gaze_X': gaze['Gaze point X'].astype(int).tolist(),
                                      'gaze_Y': gaze['Gaze point Y'].astype(int).tolist()
                                      })
-        # remove data of of the current participant from the data frame
         data = data[data['Participant name'] != participant]
-        # reset index
         data = data.reset_index(drop=True)
     return gaze_by_stimulus
 
